d65ee8ab22bbcb5b362dfd75fb42591bb9c42a59
Mefistosssss <oleksandrkondaurovit@gmail.com>
2022-01-25 20:55:17 -0800

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("!123123 hEllo world");
-
+        System.out.println("I am batch11");
+        System.out.println("I am batch12");
     }
 }
